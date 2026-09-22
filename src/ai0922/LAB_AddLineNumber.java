3f5a7f56d74ecd536aee59f2611f0e48ea69d7a1
ys2601110300 <ys2601110300@gmail.com>
2026-09-22 14:11:44 +0900

    11장. 파일 입출력 - 라인번호 출력 LAB

---
+++ src/ai0922/LAB_AddLineNumber.java
@@ -0,0 +1,34 @@
+package ai0922;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LAB_AddLineNumber {
+    public static void main(String[] args) {
+        try {
+            //BufferedReader br = new BufferedReader(new FileReader("D:/FileIOTest/myData1.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("myData1.txt"));
+            String line = "";
+            int cnt = 1;
+
+            //  File에서 한 줄 읽어오기
+            while (true)
+            {
+                line = br.readLine();
+                if (line == null)
+                    break;
+                System.out.println(cnt++ + ": " + line);
+            }
+
+            // FileInputStream 닫기
+            br.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
